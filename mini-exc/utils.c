ebe05610fbc62ad0712c5668cecb7143a8e86ea6
Houssam El-Mossaid Idrissi <hoel-mos@c4r7p7.1337.ma>
2025-04-19 14:48:26 +0100

    added the env function whos needed by builtins functions

--- mini-exc/utils.c
+++ mini-exc/utils.c
@@ -28,3 +28,31 @@ void    free_things(char *one, char **two, char *msg, int check)
         perror(msg);
     exit(1);
 }
+
+void	set_env_var(t_env **env, const char *key, const char *value)
+{
+	t_env *(current), *(prev), *(new_var);
+	current = *env;
+	prev = NULL;
+	while (current != NULL)
+	{
+		if (ft_strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = ft_strdup(value);
+			return;
+		}
+		prev = current;
+		current = current->next;
+	}
+	new_var = malloc(sizeof(t_env));
+	if (!new_var)
+		return ;
+	new_var->key = ft_strdup(key);
+	new_var->value = ft_strdup(value);
+	new_var->next = NULL;
+	if (prev != NULL)
+		prev->next = new_var;
+	else
+		*env = new_var;
+}
