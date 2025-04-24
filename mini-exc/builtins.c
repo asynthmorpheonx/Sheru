a5d4529d120875fdb5c8e3a1359d5a350c9a5947
Houssam El-Mossaid Idrissi <hoel-mos@c4r6p8.1337.ma>
2025-04-24 15:51:29 +0100

    organised builtins file

--- mini-exc/builtins.c
+++ mini-exc/builtins.c
@@ -1,51 +1,25 @@
 #include "minishell.h"
 
-void	export_print(t_env *env)
-{
-	int		i;
-	char	**tenv;
-	t_env	*tmp;
-	tenv = env_to_array(env);
-	sort_tenv(tenv);
-	i = 0;
-	while(tenv[i]) 
-	{
-		tmp = env;
-		while(tmp)
-		{
-			if(ft_strcmp(tmp->key, tenv[i]) == 0)
-			{
-				if (tmp->value)
-					printf("declare -x %s=\"%s\"\n", tmp->key, tmp->value);
-				else
-				printf("declare -x %s\n", tmp->key);
-			}
-			tmp = tmp->next;
-		}
-		i++;
-	}
-	ft_free_array(tenv);
-}
+// // export PATH+=:/new/path PATH=VALUE
 
 void	ft_export(t_cmd *cmd_list, t_env *env) // need a restructing for export's own struct that contains only its data
 {
+	int	i;
 	t_export	*data;
 
-	if(!cmd_list->cmd_flag[0])
-	{
+	data = cmd_list->export_data;
+	if (!cmd_list->eflag_check) // printing all the variables (this is done and don't ever touch it)
 		export_print(env);
-	}
-	else
+	while(data)
 	{
-		if (!data)
-			return ;
-		while(data)
-		{
-			set_env_var(&env, data->var, data->value);
-			data = data->next;
-		}
+		if (data->export_check)
+			ft_var_append(env, data->export_var, data->export_appe); // append a value to the end of a var value (handle the case where you need to append to an none exitsing var)
+		else 
+			set_env_var(&env, data->export_var, data->export_value); // modify on the var value  
+		data = data->next;
 	}
 }
+
 void	ft_unset(t_cmd *cmd_list, t_env **env)
 {
 	t_env *(prev), *(current);
@@ -76,31 +50,43 @@ void	ft_exit(t_cmd *cmd_list)
 		exit(0);
 	exit(cmd_list->code);
 }
+
+//absoult path : /home/user/Documents
+//relative path : ../Downloads / ./Downloads
+
 void	ft_cd(t_cmd *cmd_list, t_env *env)
 {
 	char	*path;
 	t_cd 	*x;
 
-	if (!(x = malloc(sizeof(t_cd))))
-		return ;
+	x = cmd_list->cd_data;
 	if (getcwd(x->oldpwd, sizeof(x->oldpwd)) != NULL)
 		set_env_var(&env, "OLDPWD", x->oldpwd);
 	else
 		perror("getcwd");
-	if(cmd_list->cmd_flag[0])
-	{
-		if (chdir(cmd_list->cmd_flag[0]) != 0)
-			perror("cd");
-	}
+	if (!x->path || x->path[0] == '\0')
+		path = get_home(env);
 	else
-	{
-		path = getenv("HOME");
-		if(chdir(path) != 0)
-			perror("cd");
-	}
+		path = x->path;
+	if (chdir(path) != 0)
+		perror("cd");
 	if (getcwd(x->pwd, sizeof(x->pwd)) != NULL)
-			set_env_var(&env, "PWD", x->pwd);
+		set_env_var(&env, "PWD", x->pwd);
 	else
 		perror("getcwd");
-	free(x);
+}
+
+void	ft_env(t_env *env)
+{
+	while (env)
+	{
+		if (env->value)
+		{
+			ft_putstr_fd(env->key, STDOUT_FILENO);
+			ft_putchar_fd('=', STDOUT_FILENO);
+			ft_putstr_fd(env->value, STDOUT_FILENO);
+			ft_putchar_fd('\n', STDOUT_FILENO);
+		}
+		env = env->next;
+	}
 }
