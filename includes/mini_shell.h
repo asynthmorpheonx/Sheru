124a340f6e0bfb0989718971760215aaae2519ca
Mohamed El mouhib <mel-mouh@e2r11p11.1337.ma>
2025-05-06 22:47:08 +0100

    included the functions

--- includes/mini_shell.h
+++ includes/mini_shell.h
@@ -6,7 +6,7 @@
 /*   By: mel-mouh <mel-mouh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 08:53:04 by mel-mouh          #+#    #+#             */
-/*   Updated: 2025/05/05 18:00:28 by mel-mouh         ###   ########.fr       */
+/*   Updated: 2025/05/06 22:10:53 by mel-mouh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,6 +71,32 @@ void	fill_with_token(char **buffer, int token_id);
 char	*safe_substr(char *str, unsigned int start, size_t len);
 char	*buffer_filler(char *line, int *i);
 char	**spliting_based_token(char *line);
+
+int		here_doc_count(int *arr, char **strs);
+char	**stor_delimiters(int *arr, char **str, int count);
+char	**heredoc_heandler(char **str, int *arr, int *mode);
+
+t_envp	*last_env(t_envp *lst);
+void	add_to_envp(t_envp **lst, t_envp *tmp);
+void	make_env(char **env, t_envp **lst, int i, int j);
+
+size_t	size_quot(char *str);
+void	remove_quote(size_t len, char **str, int i, int j);
+void	handle_quote(void);
+int	skip_quots(char *line, int *i);
+
+t_data	**box(void);
+void	ult_exit(void);
+t_envp	**envp(void);
+t_utils	*util(void);
+
+int	whichtoken(char *input, int *i);
+int	token_value(char *input);
+bool token_count(char *str);
+void	fill_with_token(char **buffer, int token_id);
+char	*buffer_filler(char *s, int *i);
+char	**spliting_based_token(char *line);
+bool	tokenize(void);
 
 
 #endif
