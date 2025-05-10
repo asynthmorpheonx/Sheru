7ac0db3835a03e82f51677e749ef32e69eabf61b
Mohamed El mouhib <mel-mouh@e2r11p11.1337.ma>
2025-05-11 00:54:40 +0100

    included some functions

--- includes/mini_shell.h
+++ includes/mini_shell.h
@@ -6,7 +6,7 @@
 /*   By: mel-mouh <mel-mouh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 08:53:04 by mel-mouh          #+#    #+#             */
-/*   Updated: 2025/05/07 16:19:05 by mel-mouh         ###   ########.fr       */
+/*   Updated: 2025/05/11 00:32:33 by mel-mouh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,26 @@
 # include <stdbool.h>
 # include <stdlib.h>
 
+# define AMBIGUOUS_REDIRECT -1
+# define RESET 0
+# define SET 1
+
 typedef enum e_token
 {
-	PIPE,
 	IND,
 	OUD,
 	APP,
+	PIPE,
 	HERDOC,
 	WORD
 }	t_token;
+
+typedef struct s_ferror
+{
+	bool	flage;
+	bool	full_exp;
+	int		error_id;
+}	t_ferror;
 
 typedef struct s_utils
 {
@@ -41,6 +52,7 @@ typedef struct s_utils
 typedef struct s_files
 {
 	char			**infile;
+	int				*i_type;
 	char			**outfile;
 	int				*o_type;
 }	t_files;
@@ -52,11 +64,10 @@ typedef struct s_envp
 	struct s_envp	*next;
 }	t_envp;
 
-
 typedef	struct s_data
 {
 	char			**cmd;
-	char			**her_doc;
+	bool			ab_redir;
 	t_files			file;
 	struct s_data	*next;
 }	t_data;
@@ -72,10 +83,6 @@ char	*safe_substr(char *str, unsigned int start, size_t len);
 char	*buffer_filler(char *line, int *i);
 char	**spliting_based_token(char *line);
 
-int		here_doc_count(int *arr, char **strs);
-char	**stor_delimiters(int *arr, char **str, int count);
-char	**heredoc_heandler(char **str, int *arr, int *mode);
-
 t_envp	*last_env(t_envp *lst);
 void	add_to_envp(t_envp **lst, t_envp *tmp);
 void	make_env(char **env, t_envp **lst, int i, int j);
@@ -83,16 +90,16 @@ void	make_env(char **env, t_envp **lst, int i, int j);
 size_t	size_quot(char *str);
 void	remove_quote(size_t len, char **str, int i, int j);
 void	handle_quote(void);
-int	skip_quots(char *line, int *i);
+int		skip_quots(char *line, int *i);
 
 t_data	**box(void);
 void	ult_exit(void);
 t_envp	**envp(void);
 t_utils	*util(void);
 
-int	whichtoken(char *input, int *i);
-int	token_value(char *input);
-bool token_count(char *str);
+int		whichtoken(char *input, int *i);
+int		token_value(char *input);
+bool	token_count(char *str);
 void	fill_with_token(char **buffer, int token_id);
 char	*buffer_filler(char *s, int *i);
 char	**spliting_based_token(char *line);
@@ -107,8 +114,14 @@ t_data	*last_node(t_data *lst);
 void	store_in_tdata(t_data **node, t_data *tmp);
 bool	stor_in_list(char **strs, int *arr, t_data **node);
 
-int	cmd_count(char **strs, int *arr);
+int		cmd_count(char **strs, int *arr);
 void	cmd_filler(char **strs, int *arr, char **buffer);
 void	cmd_flag_handle(char **strs, int *arr, t_data *node, int *mode);
+
+char	*key_value(char *key);
+
+bool	**ambiguous_ptr(void);
+
+bool	is_ifs(int c);
 
 #endif
