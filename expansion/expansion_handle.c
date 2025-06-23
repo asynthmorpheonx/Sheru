1a450e47eb33be3122461f996dc9b900713dcb5e
Mohamed El mouhib <mel-mouh@e2r11p11.1337.ma>
2025-06-24 00:26:26 +0100

    last touches & norminette

--- expansion/expansion_handle.c
+++ expansion/expansion_handle.c
@@ -6,7 +6,7 @@
 /*   By: mel-mouh <mel-mouh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 18:21:11 by mel-mouh          #+#    #+#             */
-/*   Updated: 2025/06/23 23:13:19 by mel-mouh         ###   ########.fr       */
+/*   Updated: 2025/06/24 00:25:43 by mel-mouh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,16 @@ void	expansion_util(int *ind, t_exp *ubox, int end, int tmp)
 
 bool	begin_expand(int *i, int *j, int *to)
 {
-	fetch_setter(SET, *i, false);
-	if (*to)
-		fetch_setter(SET, *i, true);
-	expand_value(i, j);
-	if (!fetcher()->error)
-		return (false);
-	util()->a[*i] = -1;
+	if (util()->s[*i][*j + 1])
+	{
+		fetch_setter(SET, *i, false);
+		if (*to)
+			fetch_setter(SET, *i, true);
+		expand_value(i, j);
+		if (!fetcher()->error)
+			return (false);
+		util()->a[*i] = -1;		
+	}
 	return (true);
 }
 
